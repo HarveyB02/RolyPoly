3a2a3f1dffd1c05dbf389d38aaa3b8640ec853f1
HarveyB02 <harvey.brett02@gmail.com>
2021-07-16 23:48:39 +1000

    Remove ping

--- events/message.js
+++ events/message.js
@@ -29,8 +29,7 @@ module.exports.execute = async (message, client) => {
             let content = words.join(' ');
             // client.tools.sendWebhook(message.channel, message.author.username, content, message.author.displayAvatarURL());
             let modChannel = await client.tools.fetchModChannel(message.guild);
-            await modChannel.send(`@here ${message.author} used profane language`);
-            modChannel.send(`>>> ${message.content}`)
+            modChannel.send(`${message.author} used profane language:\n>>> ${message.content}`);
         }
     }
 
